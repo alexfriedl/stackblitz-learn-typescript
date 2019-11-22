9fd38aa5827f3dbb28865dc1c3574dc1de4efcd0
alexfriedl <anecronika@gmx.de>
2019-11-22 16:19:39 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,24 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+class Addition {
+  public summand1: number = 5;
+  public summand2: number = 19;
+
+  constructor(summand3?: number) {
+    this.initialSum(summand3);
+  }
+
+  public initialSum(summand3): any {
+    return console.log(this.summand1 + this.summand2 + summand3);
+  }
+
+  public sum(x, y) {
+    return console.log(x + y);
+  }
+}
+
+let foo = new Addition(34);
+//foo.sum(5, 3);
+foo.initialSum(1);
